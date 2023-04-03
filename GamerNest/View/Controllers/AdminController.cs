92ddd193d0ce202fb7e54cbadd41c6f7646a7b25
Cristina CM <cristicarmat2@gmail.com>
2023-04-03 20:21:57 +0200

    LateralMenu Working

--- GamerNest/View/Controllers/AdminController.cs
+++ GamerNest/View/Controllers/AdminController.cs
@@ -4,7 +4,57 @@ namespace View.Controllers
 {
     public class AdminController :Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Articles()
+        {
+            return View();
+        }
+
+        public ActionResult Authors()
+        {
+            return View();
+        }
+
+        public ActionResult Devs()
+        {
+            return View();
+        }
+
+        public ActionResult Games()
+        {
+            return View();
+        }
+
+        public ActionResult Genres()
+        {
+            return View();
+        }
+
+        public ActionResult Languages()
+        {
+            return View();
+        }
+
+        public ActionResult Platforms()
+        {
+            return View();
+        }
+
+        public ActionResult PlayerTypes()
+        {
+            return View();
+        }
+
+        public ActionResult Publishers()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
         {
             return View();
         }
