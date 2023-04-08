f62024b6080efd41f3a3c35224ecfa19f30300d2
Cristina CM <cristicarmat2@gmail.com>
2023-04-08 22:02:11 +0200

    better Adminlogin and LateralMenu

---
+++ GamerNest/View/wwwroot/js/lateralMenu.js
@@ -0,0 +1,10 @@
+﻿const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector("#btn");
+
+const menuState = localStorage.getItem('menuState');
+sidebar.classList.toggle('open', menuState === 'open');
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    localStorage.setItem('menuState', sidebar.classList.contains('open') ? 'open' : 'closed');
+});
